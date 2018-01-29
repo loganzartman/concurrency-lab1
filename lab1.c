ac430974ea912865427cef8e5bba4df18617eada
Logan Zartman <logan.zartman@utexas.edu>
2018-01-29 16:37:20 -0600

    Added write probability

--- lab1.c
+++ lab1.c
@@ -17,11 +17,13 @@ static uint64_t shared_counter = 0;
 static uint64_t maxcounter = 100;
 static uint64_t workers = 1;
 static bool show_info = false, show_headers = false;
+static double writeProbability = 0.5;
 
 static pthread_spinlock_t spinlock;
 
 void *worker_func(void *args);
 void parse_opts(int argc, char* const argv[]);
+bool operation_is_a_write();
 
 int main(int argc, char* const argv[]) {
 	parse_opts(argc, argv);
@@ -86,18 +88,28 @@ int main(int argc, char* const argv[]) {
 	return 0;
 }
 
+bool operation_is_a_write() {
+	return (double)rand() / RAND_MAX < writeProbability;
+}
+
 void *worker_func(void *args) {
 	int id = *((int*)args);
 
-	int my_counter = 0;
+	uint64_t my_op_max = maxcounter / workers;
+	uint64_t my_counter = 0;
+	uint64_t my_operations = 0;
+
 	while (true) {
 		pthread_spin_lock(&spinlock);
-		if (shared_counter >= maxcounter) {
+		if (my_operations >= my_op_max) {
 			pthread_spin_unlock(&spinlock);
 			break;
 		}
-		++my_counter;
-		++shared_counter;
+		if (operation_is_a_write()) {
+			++my_counter;
+			++shared_counter;
+		}
+		++my_operations;
 		pthread_spin_unlock(&spinlock);
 	}
 
@@ -108,6 +120,7 @@ void parse_opts(int argc, char* const argv[]) {
 	static struct option longopts[] = {
 		{"maxcounter", required_argument, NULL, 'm'},
 		{"workers", required_argument, NULL, 'w'},
+		{"write-prob", required_argument, NULL, 'p'},
 		{"show-info", no_argument, NULL, 'i'},
 		{"show-headers", no_argument, NULL, 'H'},
 		{"help", no_argument, NULL, 'h'}
@@ -115,13 +128,16 @@ void parse_opts(int argc, char* const argv[]) {
 
 	int longindex = 0;
 	char flag = 0;
-	while ((flag = getopt_long(argc, argv, "m:w:iH", longopts, &longindex)) != -1) {
+	while ((flag = getopt_long(argc, argv, "m:w:p:iH", longopts, &longindex)) != -1) {
 		switch (flag) {
 			case 'm':
 				maxcounter = atoll(optarg);
 				break;
 			case 'w':
 				workers = atoll(optarg);
+				break;
+			case 'p':
+				writeProbability = atof(optarg);
 				break;
 			case 'i':
 				show_info = true;
--- tests.py
+++ tests.py
@@ -6,62 +6,67 @@ def main():
 	iterations = 4
 	maxworkers = 16
 	maxcounter = 10000000
+	write_prob = 0.5
 	
 	try:
-		if (len(sys.argv) == 3):
+		if (len(sys.argv) == 4):
 			maxworkers = int(sys.argv[1])
 			maxwcounter = int(sys.argv[2])
-		elif (len(sys.argv) == 2):
-			maxworkers = int(sys.argv[1])
+			write_prob = float(sys.argv[3])
 		elif (len(sys.argv) == 1):
 			pass
 		else:
 			raise
 	except:
-		print("Usage: tests.py [maxworkers] [maxcounter]")
+		print("Usage: tests.py [maxworkers] [maxcounter] [write-probability]")
 		return
 
-	test_time(maxcounter = maxcounter, maxworkers = maxworkers, iterations = iterations)
-	test_stats(maxcounter = maxcounter, maxworkers = maxworkers, iterations = iterations)
+	test_time(maxcounter = maxcounter, maxworkers = maxworkers, write_prob = write_prob,
+		iterations = iterations)
+	test_stats(maxcounter = maxcounter, maxworkers = maxworkers, write_prob = write_prob,
+		iterations = iterations)
 
-def test_time(maxcounter, maxworkers, iterations):
+def test_time(maxcounter, maxworkers, write_prob, iterations):
 	print("count: {}".format(maxcounter))
 	print("workers,time")
 
 	i = 1
 	while i <= maxworkers:
-		t = run_test_time(maxcounter = maxcounter, workers = i, iterations = iterations)
+		t = run_test_time(maxcounter = maxcounter, workers = i, write_prob = write_prob,
+			iterations = iterations)
 		print("{},{}".format(i, t))
-		i += 1
+		i *= 2
 
-def test_stats(maxcounter, maxworkers, iterations):
+def test_stats(maxcounter, maxworkers, write_prob, iterations):
 	print("count: {}".format(maxcounter))
 	print("workers,ratio,imbalance")
 
 	i = 1
 	while i <= maxworkers:
-		(ratio, imbalance) = run_test_stats(maxcounter = maxcounter, workers = i, iterations = iterations)
+		(ratio, imbalance) = run_test_stats(maxcounter = maxcounter, workers = i, 
+			write_prob = write_prob, iterations = iterations)
 		print("{},{},{}".format(i, ratio, imbalance))
-		i += 1
+		i *= 2
 
-def run_test_stats(maxcounter, workers, iterations):
+def run_test_stats(maxcounter, workers, write_prob, iterations):
 	avgRatio = 0
 	avgImbalance = 0
 
 	for i in range(0, iterations):
-		out = subprocess.check_output("./lab1 -i -m {} -w {}".format(maxcounter, workers), shell=True)
+		out = subprocess.check_output("./lab1 -i -m {} -w {} -p {}"
+			.format(maxcounter, workers, write_prob), shell=True)
 		items = out.split(b",")
 		avgRatio += float(items[0]) / iterations
 		avgImbalance += float(items[1]) / iterations
 	return (avgRatio, avgImbalance)
 
 
-def run_test_time(maxcounter, workers, iterations):
+def run_test_time(maxcounter, workers, write_prob, iterations):
 	tot = 0
 	for i in range(0, iterations):
 		out = subprocess.check_output(
-			"(time -f %e -- ./lab1 -m {} -w {}) 2>&1".format(maxcounter, workers), 
-			shell = True)
+			"(time -f %e -- ./lab1 -m {} -w {} -p {}) 2>&1"
+				.format(maxcounter, workers, write_prob), shell = True)
 		tot += float(out)
 	return tot / iterations
 
